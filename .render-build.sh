05dab612c94580c2b31fabd0523e5ec3130ee57f
unknown <2000031706@kluniversity.in>
2025-02-14 13:57:31 +0530

    Fix: Ensure JAVA_HOME and PATH are set correctly

--- .render-build.sh
+++ .render-build.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# Ensure Java 17 is used
-export JAVA_HOME=/opt/render/project/.render/java17
+# Set Java to Render's default path
+export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))
 export PATH=$JAVA_HOME/bin:$PATH
 
-# Debug: Check if Java is installed
+# Debugging: Print Java details
 echo "JAVA_HOME is set to: $JAVA_HOME"
-ls -lah $JAVA_HOME
-ls -lah $JAVA_HOME/bin
 java -version
 
 # Make Maven wrapper executable and build
