64c863d7e00c9e6f305d78f13f9047cd79428c71
unknown <2000031706@kluniversity.in>
2025-02-14 13:50:59 +0530

    Fix: Ensure JAVA_HOME is correctly set

--- .render-build.sh
+++ .render-build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 export JAVA_HOME=/opt/render/project/.render/java17
+export PATH=$JAVA_HOME/bin:$PATH
+
+echo "JAVA_HOME is set to: $JAVA_HOME"
+java -version
+
 chmod +x mvnw
 ./mvnw clean install
